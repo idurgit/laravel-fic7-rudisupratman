8814492ca7ebf26475081e677247aa4329207f4a
Rudi <126459042+idurgit@users.noreply.github.com>
2023-08-20 21:55:02 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(20)->create();
+        User::create([
+            'name' => 'Rudi S',
+            'email' => 'rudi@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'admin',
+            'phone' => '62811111111',
+            'bio' => 'flutter dev',
+            'password' => Hash::make('123456'),
+        ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'superadmin',
+            'phone' => '62811111112',
+            'bio' => 'laravel dev',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
