fe0b48d560363519331d9f75cde536a659ad4ef3
Rudi <126459042+idurgit@users.noreply.github.com>
2023-08-27 15:52:29 +0700

    upload wajib login

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
-Route::post('image/upload', [UploadController::class, 'uploadImage']);
-Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImage']);
+Route::post('image/upload', [UploadController::class, 'uploadImage'])
+    ->middleware('auth:sanctum');
+Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImage'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
